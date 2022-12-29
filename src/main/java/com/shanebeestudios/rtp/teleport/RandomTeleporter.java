b723c7acb8f69fee862e026b0c4a448e4f09638c
ShaneBee <shanebolenback@me.com>
2022-12-29 09:33:51 -0800

    RandomTeleporter - major cleanup

--- src/main/java/com/shanebeestudios/rtp/teleport/RandomTeleporter.java
+++ src/main/java/com/shanebeestudios/rtp/teleport/RandomTeleporter.java
@@ -1,11 +1,11 @@
 package com.shanebeestudios.rtp.teleport;
 
 import com.shanebeestudios.rtp.util.Utils;
-import org.bukkit.HeightMap;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.World;
+import org.bukkit.World.Environment;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -20,44 +20,14 @@ public class RandomTeleporter {
     public void rtp(Player player) {
         World world = player.getWorld();
 
-        CompletableFuture<Location> safeLocation = getSafeLocation(world);
-        safeLocation.whenComplete((location, throwable) -> {
+        getSafeLocation(world).thenApply(location -> {
             if (location != null) {
                 player.teleportAsync(location);
             } else {
-                Utils.broadcast("Couldn't find a suitable location");
+                Utils.sendMsg(player, "&cCouldn't find a suitable location!");
             }
+            return null;
         });
-    }
-
-    private CompletableFuture<Location> getRandomLocation(World world) {
-        CompletableFuture<Location> future = new CompletableFuture<>();
-        Random random = new Random();
-
-        int x = random.nextInt(100000) - 50000;
-        int z = random.nextInt(100000) - 50000;
-        Location location = new Location(world, x, 1, z);
-
-        world.getChunkAtAsync(location).thenAccept(c -> {
-            Location innerLocation = location;
-            innerLocation = world.getHighestBlockAt(innerLocation, HeightMap.MOTION_BLOCKING).getLocation();
-            innerLocation.add(0.5, 1, 0.5);
-            if (isSafe(innerLocation)) {
-                if (isOnLeaves(innerLocation)) {
-                    innerLocation = findGround(innerLocation);
-                    if (innerLocation == null) {
-                        future.cancel(true);
-                        return;
-                    }
-                }
-                Utils.broadcast("Found location...%s", innerLocation.getBlock().getRelative(BlockFace.DOWN).getType());
-                future.complete(innerLocation);
-            } else {
-                Utils.broadcast("Retry...");
-                future.cancel(true);
-            }
-        });
-        return future;
     }
 
     public CompletableFuture<Location> getSafeLocation(World world) {
@@ -70,28 +40,57 @@ public class RandomTeleporter {
         return future;
     }
 
-    private boolean isSafe(Location location) {
-        Material downType = location.getBlock().getRelative(BlockFace.DOWN).getType();
-        return downType != Material.WATER && downType != Material.LAVA;
-    }
+    private CompletableFuture<Location> getRandomLocation(World world) {
+        CompletableFuture<Location> future = new CompletableFuture<>();
+        Random random = new Random();
 
-    private boolean isOnLeaves(Location location) {
-        Material downType = location.getBlock().getRelative(BlockFace.DOWN).getType();
-        return Tag.LEAVES.isTagged(downType);
+        int x = random.nextInt(100000) - 50000;
+        int z = random.nextInt(100000) - 50000;
+        Location location = new Location(world, x, 1, z);
+
+        world.getChunkAtAsync(location).thenAccept(c -> {
+            Location safeLocation = getHighestSafeLocation(world, location);
+            if (safeLocation == null) {
+                // cancel and retry
+                future.cancel(true);
+                return;
+            }
+            safeLocation.add(0.5, 0, 0.5); // center player
+            future.complete(safeLocation);
+        });
+        return future;
     }
 
     @Nullable
-    private Location findGround(Location location) {
-        while (location.getY() > 61) {
-            location.add(0, -1, 0);
-            Block at = location.getBlock();
-            Block up = at.getRelative(BlockFace.UP);
-            Block down = at.getRelative(BlockFace.DOWN);
-            if (!at.isSolid() && !up.isSolid() && down.isSolid() && !isOnLeaves(location)) {
-                return location;
+    private Location getHighestSafeLocation(World world, Location location) {
+        Environment environment = world.getEnvironment();
+        int min = environment == Environment.NETHER ? 0 : 60;
+        int max = environment == Environment.NETHER ? 127 : 200;
+        Location loc = location.clone();
+
+        loc.setY(max);
+        while (loc.getY() > min) {
+            loc.add(0, -1, 0);
+            if (isSafe(loc)) {
+                return loc;
             }
         }
         return null;
+    }
+
+    private boolean isSafe(Location location) {
+        Block at = location.getBlock();
+        Block up = at.getRelative(BlockFace.UP);
+        Block down = at.getRelative(BlockFace.DOWN);
+        if (!at.isSolid() && !up.isSolid() && down.isSolid()) {
+            Material downType = down.getType();
+            Material atType = at.getType();
+            if (Tag.LEAVES.isTagged(downType)) {
+                return false;
+            }
+            return downType != Material.WATER && downType != Material.LAVA && atType != Material.WATER && atType != Material.LAVA;
+        }
+        return false;
     }
 
 }
