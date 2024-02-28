c9e40af18e2c14a9070a4e32000fe88ac807ba0a
ShaneBee <shanebolenback@me.com>
2024-02-27 18:55:56 -0800

    RandomTeleport - add message for when CommandAPI doesn't load

--- src/main/java/com/shanebeestudios/rtp/RandomTeleport.java
+++ src/main/java/com/shanebeestudios/rtp/RandomTeleport.java
@@ -7,26 +7,44 @@ import com.shanebeestudios.rtp.util.Utils;
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPIBukkitConfig;
 import dev.jorel.commandapi.exceptions.UnsupportedVersionException;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public class RandomTeleport extends JavaPlugin {
 
     private static RandomTeleport pluginInstance;
+    private boolean commandApiCanLoad;
     private Config config;
 
     @Override
     public void onLoad() {
         try {
             CommandAPI.onLoad(new CommandAPIBukkitConfig(this).verboseOutput(false));
+            this.commandApiCanLoad = true;
         } catch (UnsupportedVersionException ignore) {
+            this.commandApiCanLoad = false;
         }
     }
 
     @SuppressWarnings("deprecation")
     @Override
     public void onEnable() {
+        // Check if CommandAPI was loaded
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        if (!this.commandApiCanLoad) {
+            Utils.log("&eIt appears the CommandAPI is not available on your server version.");
+            Utils.log("&eThis is not a bug.");
+            Utils.log("&eThis plugin will be updated when CommandAPI supports your server version.");
+            Utils.log("&ePlugin will disable!");
+            pluginManager.disablePlugin(this);
+            return;
+        }
+
+        // Start loading plugin
         long start = System.currentTimeMillis();
+        CommandAPI.onEnable();
         pluginInstance = this;
         Utils.log("Loading...");
 
@@ -35,7 +53,6 @@ public class RandomTeleport extends JavaPlugin {
             new UpdateChecker(this);
         }
 
-        CommandAPI.onEnable();
         new RTPCommand(this);
 
         float finish = (float) (System.currentTimeMillis() - start) / 1000;
@@ -60,7 +77,6 @@ public class RandomTeleport extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        CommandAPI.unregister("randomteleport");
         CommandAPI.onDisable();
     }
 
